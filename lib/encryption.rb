32a2d510782cfb3e673c41e2cf4a247b3d8d9419
Kevin Cuadros <kev040798@gmail.com>
2020-09-18 16:24:37 -0400

    Create get keys method and get offsets method

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,8 +1,34 @@
 class Encryption
-  attr_reader :message, :key, :date
+  attr_reader :message, :key, :date, :alphabet, :non_letters, :shifts,
+  :a, :b, :c, :d
   def initialize(message, key, date)
     @message = message
     @key = key
     @date = date
+    @alphabet = ('a'..'z').to_a << ' '
+    @non_letters = []
+    @shifts = []
+    @a = 0
+    @b = 0
+    @c = 0
+    @d = 0
+  end
+
+  def get_keys
+    @a += @key[0..1].to_i
+    @b += @key[1..2].to_i
+    @c += @key[2..3].to_i
+    @d += @key[3..4].to_i
+  end
+
+  def get_offsets
+    squared = @date.to_i ** 2
+    last_four = squared.to_s.split('')
+    offset = last_four.last(4).join
+
+    @a += offset[0].to_i
+    @b += offset[1].to_i
+    @c += offset[2].to_i
+    @d += offset[3].to_i
   end
 end
