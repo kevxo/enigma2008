a88e67878af9c4dfa9e3a4c0e27c2a26f6dc0f2e
Kevin Cuadros <kev040798@gmail.com>
2020-09-18 19:25:25 -0400

    Create test for encode

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -53,20 +53,23 @@ class EncryptionTest < Minitest::Test
     encryption = Encryption.new('hello world', '02715', '040895')
 
     encryption.get_keys
-    assert_equal 2, encryption.a
-    assert_equal 27, encryption.b
-    assert_equal 71, encryption.c
-    assert_equal 15, encryption.d
-
     encryption.get_offsets
-    assert_equal 3, encryption.a
-    assert_equal 27, encryption.b
-    assert_equal 73, encryption.c
-    assert_equal 20, encryption.d
-
     encryption.add_shift
     encryption.letter_index
 
     assert_equal 8, encryption.new_shifts_array.count
+  end
+
+  def test_it_returns_array_to_encrypt
+    encryption = Encryption.new('hello world', '02715', '040895')
+
+    encryption.get_keys
+    encryption.get_offsets
+    encryption.add_shift
+    encryption.letter_index
+    encryption.new_shifts_array
+
+    expected = [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22]
+    assert_equal expected, encryption.encode
   end
 end
