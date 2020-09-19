1aac089203c2aa9bd5fa46d6d3cf35c809d29681
Kevin Cuadros <kev040798@gmail.com>
2020-09-19 15:58:59 -0400

    Update enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,11 @@
 require 'date'
 
 class Enigma
+  attr_reader :crypted
+  def initialize
+    @crypted = {}
+  end
+
   def generate_date
     today = Date.today
     new_date = today.strftime('%d/%m/%Y').delete('/')[0..5]
@@ -13,18 +18,17 @@ class Enigma
     generate
   end
 
-  def encrypt(message, key = nil, date = nil)
-    hash = {}
-    @encryption = Encryption.new(message, key, date)
-    @encryption.get_keys
-    @encryption.get_offsets
-    @encryption.add_shift
-    @encryption.letter_index
-    @encryption.new_shifts_array
-    @encryption.encode
-    hash[:encryption] = @encryption.translation
-    hash[:key] = key
-    hash[:date] = date
-    hash
+  def encrypt(message, key = generate_key, date = generate_date)
+    encryption = Encryption.new(message, key, date)
+    encryption.get_keys
+    encryption.get_offsets
+    encryption.add_shift
+    encryption.letter_index
+    encryption.new_shifts_array
+    encryption.encode
+    @crypted[:encryption] = encryption.translation
+    @crypted[:key] = key
+    @crypted[:date] = date
+    @crypted
   end
 end
