a86b7ed905d5abd89b9148bb98dd3ee2e148684d
Kevin Cuadros <kev040798@gmail.com>
2020-09-19 10:50:35 -0400

    Create attributes

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -1,6 +1,15 @@
 class Decryption
+  attr_reader :cipher_text, :key, :date, :alphabet, :shifts,
+  :a, :b, :c, :d
   def initialize(cipher_text, key, date)
-
-
+    @cipher_text = cipher_text
+    @key = key
+    @date = date
+    @alphabet = ('a'..'z').to_a << ' '
+    @shifts = []
+    @a = 0
+    @b = 0
+    @c = 0
+    @d = 0
   end
 end
