29e9d812c46edb4b28a4432da8650e63055e5cf8
Kevin Cuadros <kev040798@gmail.com>
2020-09-19 16:35:13 -0400

    Create test for decrypt with todays date

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -69,4 +69,17 @@ class EnigmaTest < Minitest::Test
 
     assert_equal expected, enigma.decrypt('keder ohulw', '02715', '040895')
   end
+
+  def test_it_decrpyts_with_todays_date
+    enigma = Enigma.new
+    encrypted = enigma.encrypt("hello world", "02715")
+    Date.stubs(:today).returns(Date.new(2020, 9, 19))
+    expected = {
+      decryption: 'hello world',
+      key: '02715',
+      date: '190920'
+    }
+
+    assert_equal expected, enigma.decrypt(encrypted[:encryption], '02715')
+  end
 end
