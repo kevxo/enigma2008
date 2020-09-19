39bc33cb593cd12da8c99bae3fb4b0d8f98a75bd
Kevin Cuadros <kev040798@gmail.com>
2020-09-19 10:48:57 -0400

    Create test for attributes

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -8,4 +8,18 @@ class DecryptionTest < Minitest::Test
 
     assert_instance_of Decryption, decryption
   end
+
+  def test_attributes
+    decryption = Decryption.new('keder ohulw', '02715', '040895')
+
+    assert_equal 'keder ohulw', decryption.cipher_text
+    assert_equal '02715', decryption.key
+    assert_equal '040895', decryption.date
+    assert_equal 27, decryption.alphabet.count
+    assert_equal [], decryption.shifts
+    assert_equal 0, decryption.a
+    assert_equal 0, decryption.b
+    assert_equal 0, decryption.c
+    assert_equal 0, decryption.d
+  end
 end
