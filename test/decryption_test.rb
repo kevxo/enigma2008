2f7b085329c7627b22a9fb163954353eafaec82d
Kevin Cuadros <kev040798@gmail.com>
2020-09-19 10:57:45 -0400

    Create test for shifts

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -22,4 +22,24 @@ class DecryptionTest < Minitest::Test
     assert_equal 0, decryption.c
     assert_equal 0, decryption.d
   end
+
+  def test_it_returns_shifts
+    decryption = Decryption.new('keder ohulw', '02715', '040895')
+
+    decryption.get_keys
+    assert_equal 2, decryption.a
+    assert_equal 27, decryption.b
+    assert_equal 71, decryption.c
+    assert_equal 15, decryption.d
+
+    decryption.get_offsets
+    assert_equal 3, decryption.a
+    assert_equal 27, decryption.b
+    assert_equal 73, decryption.c
+    assert_equal 20, decryption.d
+
+    decryption.add_shift
+
+    assert_equal [3, 27, 73, 20], decryption.shifts
+  end
 end
