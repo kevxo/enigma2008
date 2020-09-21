99cab1ce7727d4f5b63cd238ccc43885bf220c7b
Kevin Cuadros <kev040798@gmail.com>
2020-09-21 14:56:02 -0400

    Create method for given message index

--- lib/cracking.rb
+++ lib/cracking.rb
@@ -1,6 +1,6 @@
 class Crack
   attr_reader :cipher_message, :date, :alphabet, :shifts,
-  :a, :b, :c, :d
+              :a, :b, :c, :d
   def initialize(cipher_message, date)
     @cipher_message = cipher_message
     @date = date
@@ -16,6 +16,14 @@ class Crack
     last_four = @cipher_message.split('')
     letters = last_four.last(4)
     letters.map do |letter|
+      alphabet.index(letter)
+    end
+  end
+
+  def given_letter_indexes
+    given = ' end'
+    given_letters = given.split('')
+    given_letters.map do |letter|
       alphabet.index(letter)
     end
   end
