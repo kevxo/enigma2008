e0c3cf2d297d2e70ba87766aec676338c2267c11
Kevin Cuadros <kev040798@gmail.com>
2020-09-21 12:19:27 -0400

    Create attributes methods

--- lib/cracking.rb
+++ lib/cracking.rb
@@ -1,5 +1,14 @@
 class Crack
+  attr_reader :cipher_message, :date, :alphabet, :shifts,
+  :a, :b, :c, :d
   def initialize(cipher_message, date)
-
+    @cipher_message = cipher_message
+    @date = date
+    @alphabet = ('a'..'z').to_a << ' '
+    @shifts = []
+    @a = 0
+    @b = 0
+    @c = 0
+    @d = 0
   end
 end
