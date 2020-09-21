c08da81589b01e9fd45b3ffd7472e3f4206b21c2
Kevin Cuadros <kev040798@gmail.com>
2020-09-21 12:18:56 -0400

    Create attributes test

--- test/cracking_test.rb
+++ test/cracking_test.rb
@@ -1,12 +1,23 @@
 require_relative 'test_helper'
 require './lib/cracking'
-require './lib/encryption'
-require './lib/decryption'
 
 class CrackTest < Minitest::Test
   def test_it_exists
     crack = Crack.new('vjqtbeaweqihssi', '291018')
 
     assert_instance_of Crack, crack
+  end
+
+  def test_it_has_attributes
+    crack = Crack.new('vjqtbeaweqihssi', '291018')
+
+    assert_equal 'vjqtbeaweqihssi', crack.cipher_message
+    assert_equal '291018', crack.date
+    assert_equal 27, crack.alphabet.count
+    assert_equal [], crack.shifts
+    assert_equal 0, crack.a
+    assert_equal 0, crack.b
+    assert_equal 0, crack.c
+    assert_equal 0, crack.d
   end
 end
