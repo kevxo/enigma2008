9e669b923973aff425cc768059923d1e50eb1f8d
Kevin Cuadros <kev040798@gmail.com>
2020-09-22 01:41:49 -0400

    All lib refactored

--- lib/shiftable.rb
+++ lib/shiftable.rb
@@ -1,26 +1,18 @@
 module Shiftable
   def get_keys
-    @a += @key[0..1].to_i
-    @b += @key[1..2].to_i
-    @c += @key[2..3].to_i
-    @d += @key[3..4].to_i
+    keys = []
+    4.times do |index|
+      keys << @key[index..index + 1].to_i
+    end
+    keys
   end
 
   def get_offsets
     squared = @date.to_i**2
-    last_four = squared.to_s.split('')
-    offset = last_four.last(4).join
-
-    @a += offset[0].to_i
-    @b += offset[1].to_i
-    @c += offset[2].to_i
-    @d += offset[3].to_i
-  end
-
-  def add_shift
-    @shifts << @a
-    @shifts << @b
-    @shifts << @c
-    @shifts << @d
+    last_four = squared.digits.first(4)
+    keys = get_keys
+    last_four.reverse.map.with_index do |num, index|
+      num + keys[index]
+    end
   end
 end
